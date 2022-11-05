fb26a537799052e7f362331d4e7cf2f89ff72de2
Miłosz Chojnacki <s9796mc@ms.wwsi.edu.pl>
2022-11-05 12:57:40 +0100

    edit .gitignorefile

---
+++ FirmaDB/FirmaDB/FilmContext.cs
@@ -0,0 +1,25 @@
+﻿using FirmaDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaDB
+{
+    public class FilmContext : DbContext
+    {
+        public DbSet<FilmModel> Films { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptions)
+        {
+            var cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+
+            dbContextOptions.UseSqlServer(cs);
+        }
+    }
+}
